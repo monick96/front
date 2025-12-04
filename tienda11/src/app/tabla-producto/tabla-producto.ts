0cb192f75006db7dd558053f23fcc22cd83646e2
Monica <monibrave1@gmail.com>
2025-12-04 17:26:57 -0300

    formulario reaactivo que edita y guarda en memoria 3 campos del producto

--- tienda11/src/app/tabla-producto/tabla-producto.ts
+++ tienda11/src/app/tabla-producto/tabla-producto.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Productos } from '../services/productos';
 import { error, log } from 'console';
+import { ReactiveFormsModule,FormGroup,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-tabla-producto',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './tabla-producto.html',
   styleUrl: './tabla-producto.css',
 })
@@ -13,8 +14,46 @@ export class TablaProducto {
 
   productosFromApi:any[] = []
 
+  formEditarProducto = new FormGroup({
+    id:new FormControl(1),
+    title:new FormControl(""),
+    price:new FormControl(0),
+    stock:new FormControl(0)
+  })
+
   ngOnInit(){
    this.getProductosFromApi();
+  }
+
+  editarProducto(producto:any){
+    //console.log("producto: ", producto);
+    this.formEditarProducto.patchValue({
+      id:producto.id,
+      title:producto.title,
+      price:producto.price,
+      stock:producto.stock
+    })
+  }
+
+  guardarCambios(){
+    
+    let productoEditado = this.formEditarProducto.value;
+    console.log("guardando : ", this.formEditarProducto.value);
+    console.log("Guardado con exito!");
+
+    //obtener y modificar producto en memoria
+    let producto = this.getProductoById(productoEditado.id!);
+    producto.title = productoEditado.title;
+    producto.price = productoEditado.price;
+    producto.stock = productoEditado.stock;
+
+    //limpiar form
+    this.formEditarProducto.reset();
+    
+  }
+
+  getProductoById(id:number){
+    return this.productosFromApi.find(p=>p.id == id)
   }
   
   get productos(){
