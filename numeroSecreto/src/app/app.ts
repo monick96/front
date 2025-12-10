d31fe6f9dca327510194a2a3490f1773533a4e09
Monica <monibrave1@gmail.com>
2025-12-10 19:00:37 -0300

    agregando proyecto numero secreto

---
+++ numeroSecreto/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Numero Secreto');
+
+  get titleApp(){
+    return this.title(); 
+  }
+
+}
